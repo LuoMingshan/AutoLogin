8a795a25c06a9ae72adc9e5e46095c570b21beca
hbY0 <43109217+hbY0@users.noreply.github.com>
2018-12-23 16:34:43 +0800

    Update login.py

--- login.py
+++ login.py
@@ -14,3 +14,11 @@ headers_base = {
 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
 	#X-Requested-With:XMLHttpRequest
 	}
+
+def check():
+	rtn = requests.get('http://a.nuist.edu.cn/index.php/index/login')
+	info = "\u670d\u52a1\u5931\u6548\uff0c\u8bf7\u68c0\u67e5\u8d26\u6237\u53ca\u5230\u671f\u72b6\u6001"
+	flag = rtn.text.find(info)
+	if (flag != -1):
+		print "Wifi has been disconnected.\nTry to reconnect."
+		return False
